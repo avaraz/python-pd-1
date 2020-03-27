aec4de0051b51fec4ba536f7af1e70ecca595be1
kbarcikowska <barcik.kat@gmail.com>
2020-03-27 16:45:48 +0100

    Zadanie 14

---
+++ zad14.py
@@ -0,0 +1,10 @@
+slownik = {
+  "zw": "zaraz wracam",
+  "lol": "laughing out loud",
+  "jj": "juz jestem",
+  "idk": "i don't know",
+  "k": "okej"
+}
+ 
+slownik2 = slownik.copy()
+print(slownik2)
