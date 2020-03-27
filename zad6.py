c9f6f3d18278fa858fe7bdbdcb34afb1f927c8f0
kbarcikowska <barcik.kat@gmail.com>
2020-03-27 16:13:54 +0100

    Zadanie 6

---
+++ zad6.py
@@ -0,0 +1,9 @@
+txt = """And they're turning us into monsters
+Turning us into fire
+Turning us into monsters
+It's all bizarre
+It's all bizarre
+It's all bizarre"""
+ 
+x = txt.count("It's all bizarre")
+print(x)
