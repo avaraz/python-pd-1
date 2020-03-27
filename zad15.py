fc1f32f0fa65052bbf5e3a90813a361dd7500a80
kbarcikowska <barcik.kat@gmail.com>
2020-03-27 16:48:22 +0100

    Zadanie 15

---
+++ zad15.py
@@ -0,0 +1,15 @@
+slownik = {
+  "jeden": "I",
+  "dwa": "II",
+  "trzy": "III",
+  "cztery": "IV",
+  "piec": "V",
+  "szesc": "VI",
+  "siedem": "VII",
+  "osiem": "VIII",
+  "dziewiec": "IX",
+  "dziesiec": "X"
+}
+ 
+for x, y in slownik.items():
+  print(x, y)
