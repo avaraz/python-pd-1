383dde1cd81b0211b94360ce51946cc7700ed2bd
kbarcikowska <barcik.kat@gmail.com>
2020-03-27 15:34:04 +0100

    Zad 1

---
+++ zad1.py
@@ -0,0 +1,8 @@
+a,b = "Zdanie nr 1" , "Zdanie nr 2"
+print("Lancuchy: {}, {}".format(a,b))
+c,d = 34,20
+print("Liczby calkowite: {}, {} ".format(c,d))
+e,f = 19.21, 21.387
+print("Liczby rzeczywiste: {:.2f}, {:.2f}".format(e,f))
+g,h = 1+2j, 14+7j
+print("Liczby zespolone: {}, {}".format(g,h))
