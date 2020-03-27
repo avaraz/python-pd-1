c77c0922e66a7d8b201c81e0aa800456cf43d549
kbarcikowska <barcik.kat@gmail.com>
2020-03-27 16:17:14 +0100

    Zadanie 10

--- zad10.py
+++ zad10.py
@@ -1,4 +1,4 @@
 # zad 10
-films = ["The Handmaid's Tale", "Parasite", "Mommy", "Nightcrawler", "The Green Mile"]
+films = ["The Handmaiden", "Parasite", "Mommy", "Nightcrawler", "The Green Mile"]
 films.sort()
 print(films)
