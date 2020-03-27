e75930d882281eacf5d0d30699d8dfcd2f0fd376
kbarcikowska <barcik.kat@gmail.com>
2020-03-27 16:07:06 +0100

    Zadanie 4

---
+++ zad4.py
@@ -0,0 +1,8 @@
+from math import *
+print("e^10 =",e*10)
+x = log(5+(sin(8))**2,e)**(1/6)
+print("6\u2713ln(5+sin\u00B28) =", x)
+y = float(fabs(3.55))
+z = float(fabs(4.80))
+print("|3.55| =",y)
+print("|4.80| = {:.2f}".format(z))
