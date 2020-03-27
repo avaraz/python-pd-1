391a4b6957305839e6cf0cc0f149355b01262070
kbarcikowska <barcik.kat@gmail.com>
2020-03-27 16:14:38 +0100

    Zadanie 8

---
+++ zad8.py
@@ -0,0 +1,5 @@
+# zad 8
+txt = "And they're turning us into monsters Turning us into fire Turning us into monsters It's all bizarre It's all bizarre It's all bizarre"
+ 
+x = txt.split(" ")
+print(x)
