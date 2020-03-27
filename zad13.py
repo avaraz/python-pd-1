7ef86cdf81c8a8b2ba255f3a1d6c7821f31d47b5
kbarcikowska <barcik.kat@gmail.com>
2020-03-27 16:44:54 +0100

    Zadanie 13

---
+++ zad13.py
@@ -0,0 +1,14 @@
+slownik = {
+  "Biały": "Bartosz Białczak",
+  "Krystek": "Krystian Cabaj",
+  "Paracetamol": "Tomasz Pisarewski",
+  "Łysy": "Kamil Marteńczuk",
+  "Guzik": "Oliwia Guźniczak",
+  "Kowi": "Piotr Kowalski",
+  "Patyś": "Patryk Szczygielski",
+  "Abes": "Sebastian Bałdyga",
+  "Wiśnia": "Monika Wisniewska",
+  "Łuki": "Łukasz Liberna"
+}
+ 
+print(slownik.get("Biały"), slownik.get("Krystek"), slownik.get("Paracetamol"))
