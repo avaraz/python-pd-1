4b7edf526959f41226e3725156d869446702cf82
kbarcikowska <barcik.kat@gmail.com>
2020-03-27 16:56:18 +0100

    Zadanie 16

---
+++ zad16.py
@@ -0,0 +1,10 @@
+slownik = {
+    "Night in the woods": "2017",
+    "What Remains of Edith Finch": "2017",
+    "Skyrim": "2011",
+    "Wiedźmin 3": "2015",
+    "Grand Theft Auto San Andreas": "2004",
+    "Mass Effect 3": "2012"
+}
+
+print(len(slownik))
